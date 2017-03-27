d3734bb3938553e9cca5ab523cb540df60e7c885
Tsumura Hiromasa <tsumurahiromasa@Tsumura-no-MacBook-Pro.local>
2017-03-27 14:09:04 +0900

    3.27.14:08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
   def show
-    @user = current_user
-    @videos = Video.where(user_id: current_user.id).order("created_at DESC")
+    @user = User.find(params[:id])
+    @videos = Video.where(user_id: @user.id).order("created_at DESC")
     @yt_video_ids_j = @videos.map {|video| video.yt_video_id}.to_json.html_safe
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root  'posts#index'                       #ルートパスを指定
   resources :posts, only: [:index, :create, :destroy, :edit, :update, :show] do
-    resources :comments, only: [:create]
+    resources :comments, only: :create
   end
-  get   'users/:id'   =>  'users#show'      #Mypageへのルーティング
+  resources :users, only: :show
 end
